117237c232cf72cc1f3ced4b8f5043a3585d2ece
TobyFang2010 <yjnsfzt@outlook.com>
2026-08-17 20:15:43 +0800

    修改一些输出描述
    Updated welcome message and instructions for calculator.

--- count.py
+++ count.py
@@ -1,14 +1,13 @@
 ﻿import math
 import random
 print("""
-欢迎使用简易计算器
-你可以在">>>"后自由计算
-支持输入算式
-支持用math和random模块
-如:math.sqrt(2)
-输入完算是按下回车
-cmd格式输出答案
-快来体验吧
+欢迎使用简易计算器,
+你可以在'>>>'后自由计算,
+支持输入算式,
+已导入math和random模块,
+如:math.sqrt(2),
+输入exit()退出.
+快来体验吧!
 """)
 while True:
     a = input(">>>")
